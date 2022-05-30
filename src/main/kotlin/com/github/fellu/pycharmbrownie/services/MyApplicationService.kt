6eb9c08e8be469354a7b40e87bf145638e5bf652
GitHub Action <action@github.com>
2022-05-30 12:24:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/fellu/pycharmbrownie/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/fellu/pycharmbrownie/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.fellu.pycharmbrownie.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fellu.pycharmbrownie.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/fellu/pycharmbrownie/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fellu/pycharmbrownie/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.fellu.pycharmbrownie.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fellu.pycharmbrownie.MyBundle
 
 class MyProjectService(project: Project) {
 
